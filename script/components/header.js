d662f707dd07548639d142535fa79770c071879f
Emmelin Larina Tvedt Nilsen <emmelin.tvedt@gmail.com>
2026-08-31 16:59:26 +0200

    Add transition effects to header navigation links and update icon toggle

--- script/components/header.js
+++ script/components/header.js
@@ -1,7 +1,7 @@
 export function renderHeader() {
   const header = document.getElementById("header");
 
-  header.innerHTML = `
+  header.innerHTML = /*html*/ `
     <nav 
     class="flex items-center justify-between bg-primary text-white px-4 py-3"
     aria-label="Main navigation"
@@ -25,9 +25,26 @@ export function renderHeader() {
       </button>
 
       <div class="hidden md:flex md:items-center md:gap-6">
-        <a href="#venue-list" class="block py-2 text-white">Venues</a>
-        <a href="./login" class="block py-2 text-white">Login</a>
-        <a href="./register" class="block py-2 text-white">Register</a>
+        <a 
+        href="#venue-list" 
+        class="transition-opacity hover:opacity-75 text-white"
+        >
+        Venues
+      </a>
+
+        <a 
+        href="./login" 
+        class="transition-opacity hover:opacity-75 block py-2 text-white"
+        >
+        Login
+        </a>
+        <a 
+        href="./register" 
+        class="transition-opacity hover:opacity-75 block py-2 text-white"
+        >
+        Register
+        </a>
+
       </div>
     </nav>
 
@@ -43,11 +60,14 @@ export function renderHeader() {
 
   const menuButton = header.querySelector("button");
   const mobileMenu = document.getElementById("mobile-menu");
+  const menuIcon = menuButton.querySelector("i");
 
   menuButton.addEventListener("click", () => {
     const isOpen = menuButton.getAttribute("aria-expanded") === "true";
 
     menuButton.setAttribute("aria-expanded", String(!isOpen));
     mobileMenu.classList.toggle("hidden");
+    menuIcon.classList.toggle("fa-bars");
+    menuIcon.classList.toggle("fa-xmark");
   });
 }
